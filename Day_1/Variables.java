9bf50564b91760f6a7c713e8a803ea11bd1eddb9
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-03 18:35:53 +0530

    first day of learning new programming language and understand the logic

---
+++ Day_1/Variables.java
@@ -0,0 +1,13 @@
+ public class Variables{
+   public static void main(String[] args) {
+     //Variables in java...
+     byte b = 127;
+     System.out.println(b);
+     int i = 56;
+     System.out.println(i);
+     char ch = 'a';
+     System.out.println(ch);
+     String s = "Prem mon";
+     System.out.println(i);
+   }
+ }
---
+++ Day_1/javaBasics.java
@@ -0,0 +1,22 @@
+public class javaBasics{
+  public static void main(String[] args) {
+    /* Output in java... */
+    System.out.print("hello world\n");
+    System.out.println("hello world");
+    System.out.println("****");
+    System.out.println("***");
+    System.out.println("**");
+    System.out.println("*");
+    //variables...
+    int a = 5;
+    System.out.println(a);
+    int b = 7;
+    System.out.println(b);
+    int area = 2 * ( a + b);
+    System.out.println(area);
+    String name = "Prem";
+    System.out.println(name);
+    
+  }
+
+}
