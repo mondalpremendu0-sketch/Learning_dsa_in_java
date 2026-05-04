5b007d9a9b61f34efc55b3da8cdb587974f7e90c
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-04 15:53:59 +0530

    Assign Opearator completed

---
+++ Day_2/Operators/assigneOperators.java
@@ -0,0 +1,18 @@
+public class assigneOperators{
+  public static void main(String[] args) {
+   // "=","+=","-=","*=","/=","%="
+   int a = 10;
+   a = 34;
+   System.out.println(a);
+   a += 40;//a = a + 40;
+   System.out.println(a);
+   a -= 40;//a = a - 40;
+   System.out.println(a);
+   a *= 40;//a = a * 40;
+   System.out.println(a);
+   a /= 40;//a = a / 40;
+   System.out.println(a);
+   a %= 40;//a = a % 40;
+   System.out.println(a);
+  }
+}
