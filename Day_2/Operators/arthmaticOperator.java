3db0a3f6b40555f6332216c24ea1e24684b4d30e
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-04 14:27:26 +0530

    arthematic operators done

---
+++ Day_2/Operators/arthmaticOperator.java
@@ -0,0 +1,14 @@
+public class arthmaticOperator{
+  public static void main(String[] args) {
+    //"+" ,"-", "*", "/"
+    int a = 10,b = 20,sum,diff,multi,divi;
+    sum = a + b;
+    diff = a - b;
+    multi = a * b;
+    divi = a / b;
+    System.out.println(sum);
+    System.out.println(diff);
+    System.out.println(multi);
+    System.out.println(divi);
+  }
+}
