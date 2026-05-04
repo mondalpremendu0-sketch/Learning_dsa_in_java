fcd84c1c6e1c9d9ff541ba40ba48fae214696ad4
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-04 16:13:06 +0530

    Conditional operators complete

---
+++ Day_2/Operators/conditionalOperator.java
@@ -0,0 +1,14 @@
+public class conditionalOperator{
+  public static void main(String[] args) {
+   // "==",">","<",">=","<=","!="
+   //it return a Boolean type data
+   int A = 5,B = 9;
+   System.out.println((A == B));
+   System.out.println((A != B));
+   System.out.println((A <= B));
+   System.out.println((A >= B));
+   System.out.println((A > B));
+   System.out.println((A < B));
+   
+  }
+}
