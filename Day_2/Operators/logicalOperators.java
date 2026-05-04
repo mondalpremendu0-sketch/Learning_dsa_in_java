8757e232f30656c8536addb2192a3036c669af5f
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-04 16:36:35 +0530

    Logical operator done

---
+++ Day_2/Operators/logicalOperators.java
@@ -0,0 +1,11 @@
+public class logicalOperators{
+  public static void main(String[] args) {
+    // Logical and =>> "&&"
+    System.out.println((3 > 2) && (8>5));
+    // Logical or =>> "||"
+    System.out.println((3 > 2) || (8>5));
+    // Logical or =>> "!"
+   System.out.println(!(3 > 2));
+    
+  }
+}
