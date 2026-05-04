0613530909ac48ba3d208cd524eb0fb36e1cf052
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-04 16:46:37 +0530

    question 1 and 2 done

---
+++ Day_2/Question/Test.java
@@ -0,0 +1,40 @@
+public class Test{
+  public static void main(String[] args) {
+   
+   //1. Question no - 1
+    int p = 2, q = 5;
+    int exp1 = (p * q / p);
+    int exp2 = (p * (q / p));
+
+    System.out.print(exp1 + ", ");
+    System.out.print(exp2);
+    
+   //2. Question no - 2
+   
+    int x = 200, y = 50, z = 100;
+
+    if(x > y && y > z){
+        System.out.println("Hello");
+    }
+    
+    if(z > y && z < x){
+        System.out.println("Java");
+    }
+    
+    if((y+200) < x && (y+150) < z){
+        System.out.println("Hello Java");
+    }
+    
+   //3. Question no - 3
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+  }
+}
