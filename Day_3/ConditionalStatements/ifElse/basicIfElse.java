24e0ab4bafbf78494690e5eb415d96edc877fe77
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-06 01:41:41 +0530

    basic if else and syntax of if else

---
+++ Day_3/ConditionalStatements/ifElse/basicIfElse.java
@@ -0,0 +1,15 @@
+public class basicIfElse{
+  public static void main(String[] args) {
+    // Syntax =>> this is called conditionl reering
+    // if (condition) {code}
+    // else{code}
+    
+    int userAge = 23;
+    if (userAge > 18) {
+      System.out.print("You are Audalt");
+    }
+    else {
+      System.out.print("You are not adult");
+    }
+  }
+}
