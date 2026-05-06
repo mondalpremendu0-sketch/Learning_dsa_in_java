4d0e2803ddb11a4302b283aa7c8044fa2a136de9
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-06 18:32:43 +0530

    learned ternary operator

---
+++ Day_3/ConditionalStatements/TernaryOperator/ternaryOperator.java
@@ -0,0 +1,11 @@
+public class ternaryOperator{
+  public static void main(String[] args) {
+    int b = 36 , a = 63;
+    
+    boolean x = (a < b ) ? true : false;
+    int y = (a > b) ? a : b;
+    
+    System.out.println(x);
+    System.out.print(y);
+  }
+}
