3b2c79aa5d9cf5112f613542d913b38caf7a2707
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-16 14:10:06 +0530

    learn do while to loop

---
+++ Day_5/doWhileLoop/basicDoWhileLoop.java
@@ -0,0 +1,10 @@
+public class basicDoWhileLoop{
+  public static void main(String[] args) {
+    //syntax:
+    int i = 5;
+    do{
+      System.out.println("hello");
+      i++;
+    }while(i < 7);
+  }
+}
