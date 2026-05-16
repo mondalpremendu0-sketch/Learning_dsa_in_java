81fc9f28e4623b9e766ba5bed5d9ac296886a832
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-16 13:42:46 +0530

    learn about syntax

---
+++ Day_5/whileLoop/basicWhileLoop.java
@@ -0,0 +1,10 @@
+public class basicWhileLoop{
+  public static void main(String[] args) {
+    //Syntax===>>
+    int i = 1;
+    while(i < 5){
+      System.out.println("hello");
+      i++;
+    }
+  }
+}
