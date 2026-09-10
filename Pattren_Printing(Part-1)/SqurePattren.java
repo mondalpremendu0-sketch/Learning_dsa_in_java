54c14c1d00f07a528c5d6eaf3df17b2796132838
mondalpremendu0-sketch <mondalpremendu@0gmail.com>
2026-09-11 01:28:43 +0530

    add Square Pattern printing program

---
+++ Pattren_Printing(Part-1)/SqurePattren.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+
+public class SqurePattren {
+    public static void main(String[] args) {
+       //Square Pattren--
+
+       System.out.print("Enter a Positive Number: ");
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+
+       for (int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++){
+
+            System.out.print(
+                '*'
+            );
+
+        }
+        System.out.println();        
+       }
+
+
+       //Right_Angle Triangle---
+
+       for (int i = 0; i < n; i++) {
+        for(int j = 0; j <= i; j++){
+
+            System.out.print(
+                '*'
+            );
+
+        }
+        System.out.println();        
+       }
+
+
+       //inverted Triangle-----
+
+            for (int i = 0; i < n; i++) {
+        for(int j = n-i; j > 0; j--){
+
+            System.out.print(
+                '*'
+            );
+
+        }
+        System.out.println();        
+       }
+
+    }
+}
