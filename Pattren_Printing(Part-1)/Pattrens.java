fb5c6290f3cd82124046b3761b0eb0e39bb31a10
mondalpremendu0-sketch <mondalpremendu@0gmail.com>
2026-09-12 12:25:35 +0530

    add pattern printing programs for various shapes

--- Pattren_Printing(Part-1)/Pattrens.java
+++ Pattren_Printing(Part-1)/Pattrens.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 
-public class SqurePattren {
+public class Pattrens {
     public static void main(String[] args) {
        //Square Pattren--
 
---
+++ Pattren_Printing(Part-2)/Pattrens.java
@@ -0,0 +1,148 @@
+
+public class Pattrens {
+    public static void main(String[] args){
+    
+      //HOLLOW RECTANGLE PATTREN.....
+        int line = 4;
+        int cols = 4;
+
+        for (int i = 1; i <= line; i++) {
+
+            for(int j = 1; j <= cols; j++){
+                if(i == 1 || j == 1 || i == line || j == cols){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+            
+        }
+
+      //INVERTED & ROTATED HALF-PYRAMID.....
+
+      for(int i =1; i <= line; i++){
+        for(int j = 1; j <= line-i; j++){
+            System.out.print(" ");
+
+        }for(int j = 1; j <= i; j++){
+            System.out.print("*");
+
+        }
+
+        System.out.println();
+
+      }
+
+
+     // INVERTED HALF-PYRAMID wŤth Numbers pattern...... 
+
+     for(int i = 1; i <= 5; i++){
+
+        for(int j = 1; j <= 5-i+1; j++){
+            System.out.print(j);
+        }
+        System.out.println();
+     }
+
+     //FLOYD'S TrŤangle pattern.......
+
+     int a = 1;
+     for(int i = 1; i <= 5; i++){
+        for(int j = 1; j <= i; j++){
+            System.out.print(a);
+            a++;
+        }
+        System.out.println();
+     }
+
+     //0-1 TrŤangle pattern.......
+        // 1
+        // 01
+        // 101
+        // 0101
+        // 10101
+
+     for(int i = 1; i <= 5; i++){
+        
+        for(int j = 1; j <= i; j++){
+            if (i % 2 == 0 && j % 2 == 0) {
+                System.out.print('0');
+            }else{
+                System.out.print('1');
+
+            }
+        }
+        System.out.println();
+
+
+     }
+
+     // BUTTERFLY pattern .......
+
+     //upper half
+     for(int i = 1; i <= 5; i++){
+        for(int j = 1; j <= i; j++){
+            System.out.print("*");
+        }
+        for(int j = 1; j <= 2*5 - 2*i; j++){
+            System.out.print(" ");
+        }
+        for(int j = 1; j <= i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+     }
+
+     //lower half
+     for(int i = 1; i <= 5; i++){
+        
+        for(int j = 1; j <= 5-i; j++){
+            System.out.print("*");
+        }
+        for(int j = 1; j <= 2*i; j++){
+            System.out.print(" ");
+        }
+        for(int j = 1; j <= 5-i; j++){
+            System.out.print("*");
+        }
+
+        System.out.println();
+     }
+
+     //SOLID RHOMBUS pattern.......
+
+     for(int i = 1; i <= line; i++){
+        for(int j = 1; j <= line-i;j++){
+            System.out.print(" ");
+        }
+        for(int j = 1; j <= cols; j++){
+            System.out.print("*");
+        }
+
+        System.out.println();
+     }
+
+     //HOLLOW RHOMBUS........
+
+     for(int i = 1; i <= line; i++){
+
+        for(int j = 1; j <= line-i; j++){
+            System.out.print(" ");
+        }
+        for(int j = 1; j <= cols; j++){
+           if (i == 1 || j == 1 || i == line || j == cols) {
+                System.out.print("*");
+           }else {
+            System.out.print(" ");
+           }
+
+        }
+        System.out.println();
+     }
+
+
+    }
+    
+}
