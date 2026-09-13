ccfd2c5bc1d60e95f1d17ec562b326679128cd84
mondalpremendu0-sketch <mondalpremendu@0gmail.com>
2026-09-13 23:10:27 +0530

    fix pattern logic in 0-1 triangle pattern for correct output

--- Pattren_Printing(Part-2)/Pattrens.java
+++ Pattren_Printing(Part-2)/Pattrens.java
@@ -67,10 +67,10 @@ public class Pattrens {
      for(int i = 1; i <= 5; i++){
         
         for(int j = 1; j <= i; j++){
-            if (i % 2 == 0 && j % 2 == 0) {
-                System.out.print('0');
-            }else{
+            if ((i+j) % 2 == 0) {
                 System.out.print('1');
+            }else{
+                System.out.print('0');
 
             }
         }
