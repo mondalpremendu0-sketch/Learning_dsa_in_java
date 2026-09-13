c708a7a6091201cc6fc37c32c78a2dc1c85f62f4
mondalpremendu0-sketch <mondalpremendu@0gmail.com>
2026-09-13 19:17:44 +0530

    refactor Assignment.java for improved readability and functionality

--- functions(Part-2)/Assignments/Assignment.java
+++ functions(Part-2)/Assignments/Assignment.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Assignment {
 
-    public static void average_of_Three(int a,int b,int c) {
-        
+    public static void average_of_Three(int a, int b, int c) {
+
         float avg = (a + b + c) / 3;
 
         System.out.println("Avaarege of " + a + b + c + " = " + avg);
@@ -11,14 +11,14 @@ public class Assignment {
     }
 
     public static void Palindrome_check(int n) {
-        
+
         int original = n;
         int rev = 0;
 
-        while (n > 0) {
+        while (n != 0) {
 
             int rem = n % 10;
-            rev = rev + rem * 10;
+            rev = rem + (rev * 10);
             n /= 10;
         }
 
@@ -31,31 +31,44 @@ public class Assignment {
 
     }
 
-    public static boolean isEven(int n){
+    public static boolean isEven(int n) {
         if (n % 2 == 0) {
             return true;
-        }else return false;
+        } else
+            return false;
     }
 
+    public static int sum_of_Digits(int n) {
+
+        int sum = 0;
+        while (n > 0) {
+
+            int rem = n % 10;
+            sum += rem;
+            n /= 10;
+
+        }
+        return sum;
+
+    }
 
     public static void main(String[] args) {
 
         // Question 1 : Write a Java method to compute the average of three numbers...
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        // System.out.print("enter your 1st number: ");
-        // int a = sc.nextInt();
+        System.out.print("enter your 1st number: ");
+        int a = sc.nextInt();
 
-        // System.out.print("enter your 2nd number: ");
-        // int b = sc.nextInt();
+        System.out.print("enter your 2nd number: ");
+        int b = sc.nextInt();
 
-        // System.out.print("enter your 3rd number: ");
-        // int c = sc.nextInt();
-        // average_of_Three(a,b,c);
+        System.out.print("enter your 3rd number: ");
+        int c = sc.nextInt();
+        average_of_Three(a,b,c);
 
-
-
-        // Write a Java program to check if a number is a palindrome in Java? ( 121 is
+        // Question 2 : Write a Java program to check if a number is a palindrome in
+        // Java? ( 121 is
         // a palindrome, 321 is not)
 
         System.out.print("enter your 1st number: ");
@@ -63,11 +76,27 @@ public class Assignment {
 
         Palindrome_check(n);
 
-        // Write a method named isEven that accepts an int argument. The method 
-        // should return true if the argument is even, or false otherwise. Also write a program to test 
+        // Question 3 : Write a method named isEven that accepts an int argument. The
+        // method
+        // should return true if the argument is even, or false otherwise. Also write a
+        // program to test
         // your method.
-       // isEven();
 
+        System.out.print("enter your number: ");
+        int x = sc.nextInt();
+
+        if (isEven(x)) {
+        System.out.println("Number is Even!!");
+        }else{
+        System.out.println("Number is not Even!!");
+        }
+
+        // Question 4 : Write a Java method to compute the sum of the digits in an
+        // integer...
+        
+        System.out.print("enter your number: ");
+        int y = sc.nextInt();
+        System.out.println("Sum = "+sum_of_Digits(y)); 
     }
 
 }
