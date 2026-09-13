5f98b8e145622a0b503aeaf9d62a7ce4952b3fff
mondalpremendu0-sketch <mondalpremendu@0gmail.com>
2026-09-13 09:08:04 +0530

    add factorial, prime check, and binary conversion functions

--- functions(Part-1)/BasicsQuestions/questions.java
+++ functions(Part-1)/BasicsQuestions/questions.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.lang.Math;
 
 public class questions {
     // Question 1---------
@@ -16,6 +17,7 @@ public class questions {
         System.out.println(product);
     }
 
+    // Question 2---------
     public static int Factorial(int n) {
         int fact = 1;
         for (int i = 2; i <= n; i++) {
@@ -26,8 +28,9 @@ public class questions {
 
     }
 
-    public static void NCR(){
-        //nCr = n!/r!*(n-r)!
+    // Question 3---------
+    public static void NCR() {
+        // nCr = n!/r!*(n-r)!
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter (n): ");
@@ -36,27 +39,135 @@ public class questions {
         System.out.print("Enter (r): ");
         int r = sc.nextInt();
 
-        int nCr = Factorial(n)/(Factorial(r) * Factorial(n-r));
+        int nCr = Factorial(n) / (Factorial(r) * Factorial(n - r));
         System.out.print("nCr = ");
         System.out.println(nCr);
 
-
     }
 
-    public static void main(String[] args) {
-        product_of_ab();
-
+    // Question 4---------
+    public static void prime_check() {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter (n): ");
         int n = sc.nextInt();
 
-        int fact = Factorial(n);
-        System.out.print("factorial = ");
-        System.out.println(fact);
+        boolean flag = false;
 
+        if (n < 2) {
+            System.out.println("Its a PRIME Number!");
+            return;
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                flag = true;
+            }
 
-        NCR();
+        }
+
+        if (flag) {
+            System.out.println("Its a Prime Number");
+        } else {
+            System.out.println("Its a PRIME Number!");
+        }
+
+    }
+
+    // Question 5---------
+    public static void prime_in_range() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter 1st range: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter 2nd range: ");
+        int end = sc.nextInt();
+
+        if (start == 0) {
+            System.out.println("0 is neither Prime nor Composite!");
+            return;
+        }
+
+        for (int i = start; i <= end; i++) {
+            int flag = 0;
+
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    flag = 1;
+                    break;
+                }
+
+            }
+            if (flag == 0) {
+                System.out.print(i);
+
+            }
+
+        }
+
+    }
+
+    // Question 6---------
+    public static void Binary_to_decimal() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Your Binary Number: ");
+        int binary = sc.nextInt();
+
+        int decimal = 0;
+        int idx = 0;
+        while (binary > 0) {
+            int rem = binary % 10;
+            binary /= 10;
+            decimal += rem * Math.pow(2, idx);
+            idx++;
+
+        }
+        System.out.print("The Converted Decimal Number = ");
+        System.out.print(decimal);
+    }
+
+    // Question 7---------
+    public static void decimal_to_binary() {
+
+        int decimal = 16;
+        String sbinary = "";
+
+        while (decimal > 0) {
+            int rem = decimal % 2;
+            decimal /= 2;
+            sbinary += rem;
+        }
+
+        String Binary = "";
+        for (int i = sbinary.length()-1; i >= 0; i--) {
+
+            Binary += sbinary.charAt(i);
+
+        }
+
+        System.out.println(Binary);
+
+    }
+
+    public static void main(String[] args) {
+        // product_of_ab();
+
+        // Scanner sc = new Scanner(System.in);
+
+        // System.out.print("Enter (n): ");
+        // int n = sc.nextInt();
+
+        // int fact = Factorial(n);
+        // System.out.print("factorial = ");
+        // System.out.println(fact);
+
+        // NCR();
+        // prime_check();
+        // prime_in_range();
+        // Binary_to_decimal();
+        decimal_to_binary();
 
     }
 
