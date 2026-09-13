5964e10eb487439b017d726c41c8b121624bcbd4
mondalpremendu0-sketch <mondalpremendu@0gmail.com>
2026-09-14 00:25:09 +0530

    add palindromic pattern with numbers to Pattrens.java

--- Pattren_Printing(Part-2)/Pattrens.java
+++ Pattren_Printing(Part-2)/Pattrens.java
@@ -148,8 +148,7 @@ public class Pattrens {
         // ---2 2 
         // --3 3 3 
         // -4 4 4 4 
-        // 5 5 5 5 5 
-
+        // 5 5 5 5 5
 
     for(int i = 1; i <= 5; i++){
         for(int j = 1; j <= 5 - i; j++){
@@ -162,7 +161,28 @@ public class Pattrens {
     }
  
  
- 
+    //PALINDROMIC Pattern wŤth Numbers pattern 
+    //         1 
+    //       2 1 2 
+    //     3 2 1 2 3 
+    //   4 3 2 1 2 3 4 
+    // 5 4 3 2 1 2 3 4 5 
+
+    for(int i = 1; i <= 5; i++){
+        
+        for(int j = 1; j <= 5-i; j++){
+            System.out.print(" ");
+        }
+
+        for(int j = i; j >= 1; j--){
+            System.out.print(j + " ");
+        }
+        for(int j = 2; j <= i; j++){
+            System.out.print(j + " ");
+        }
+
+        System.out.println();
+    }
  
 
  
