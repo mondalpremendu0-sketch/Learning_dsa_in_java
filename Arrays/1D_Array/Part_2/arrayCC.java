f8c0f3b664fc4308072adb9613aeeb6bb0695ac0
mondalpremendu0-sketch <mondalpremendu@0gmail.com>
2026-09-14 19:53:10 +0530

    refactor arrayCC class: rename Print_SubArray method and add Sum_of_SubArray and updated Max_Subarray methods

--- Arrays/1D_Array/Part_2/arrayCC.java
+++ Arrays/1D_Array/Part_2/arrayCC.java
@@ -12,12 +12,12 @@ public class arrayCC {
 
     }
 
-    //Print SubArray.......
-    public static void Print_SubArray(int arr[]) {
+    // Print SubArray.......
+    public static void Print_SubArrays(int arr[]) {
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j++) {
-                for(int k = i; k <= j; k++){
+                for (int k = i; k <= j; k++) {
                     System.out.print(arr[k] + " ");
                 }
                 System.out.println();
@@ -26,12 +26,52 @@ public class arrayCC {
         }
     }
 
+    //Sum of SubArray.....
+    public static void Sum_of_SubArray(int arr[]) {
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += arr[k];
+                }
+                System.out.println();
+                System.out.print("Sum = " + sum);
+
+            }
+            System.out.println();
+        }
+    }
+
+    //Max Sum of subArray.........
+    public static void Max_Subarray(int arr[]) {
+        int currentSum = 0;
+        int MaxSum = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                currentSum = 0;
+                for (int k = i; k <= j; k++) {
+                    currentSum += arr[k];
+                }
+                if (MaxSum < currentSum) {
+                    MaxSum = currentSum;
+                }
+                System.out.print("Sum = " + currentSum);
+
+            }
+            System.out.println();
+        }
+        System.out.println("The Max = " + MaxSum);
+    }
+
     public static void main(String args[]) {
 
         int nums[] = { 2, 4, 6, 8, 10 };
-        //Pairs(nums);
-        Print_SubArray(nums);
-
+        // Pairs(nums);
+        // Print_SubArrays(nums);
+        //Sum_of_SubArray(nums);
+        Max_Subarray(nums);
     }
 
 }
